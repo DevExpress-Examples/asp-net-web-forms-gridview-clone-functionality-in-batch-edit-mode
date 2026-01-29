c80ae48f9a7f4453c75c69dd2cbceff27e164208
DevExpressExampleBot <fakeEmail>
2026-01-29 16:25:56 +0400

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -22,7 +22,7 @@ Partial Public Class _Default
 					.C5 = New DateTime(2013 + i, 12, 16)
 				}).ToList()
 			End If
-			Return CType(Session(key), List(Of GridDataItem))
+			Return DirectCast(Session(key), List(Of GridDataItem))
 		End Get
 	End Property
 	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
@@ -69,14 +69,16 @@ Partial Public Class _Default
 		Return item
 	End Function
 	Protected Function UpdateItem(ByVal keys As OrderedDictionary, ByVal newValues As OrderedDictionary) As GridDataItem
-		Dim id = Convert.ToInt32(keys("ID"))
-		Dim item = GridData.First(Function(i) i.ID = id)
+'INSTANT VB NOTE: The variable id was renamed since Visual Basic does not handle local variables named the same as class members well:
+		Dim id_Conflict = Convert.ToInt32(keys("ID"))
+		Dim item = GridData.First(Function(i) i.ID = id_Conflict)
 		LoadNewValues(item, newValues)
 		Return item
 	End Function
 	Protected Function DeleteItem(ByVal keys As OrderedDictionary, ByVal values As OrderedDictionary) As GridDataItem
-		Dim id = Convert.ToInt32(keys("ID"))
-		Dim item = GridData.First(Function(i) i.ID = id)
+'INSTANT VB NOTE: The variable id was renamed since Visual Basic does not handle local variables named the same as class members well:
+		Dim id_Conflict = Convert.ToInt32(keys("ID"))
+		Dim item = GridData.First(Function(i) i.ID = id_Conflict)
 		GridData.Remove(item)
 		Return item
 	End Function
